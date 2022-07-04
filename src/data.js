6faa35ce1b1617d43e44ea8388a1ff0e82c1894e
abdul720 <73010648+abdul720@users.noreply.github.com>
2022-07-03 16:59:24 -1200

    first commit

---
+++ src/data.js
@@ -0,0 +1,70 @@
+const list = [
+  {
+    id: 1,
+    title: "Cabage Salad",
+    price: 89,
+    img: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/300x300/exps9035_RM10002C45D.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Roast Potatoes",
+    price: 98,
+    img: "https://cdn.pixabay.com/photo/2015/04/13/18/51/fried-721080__340.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Daal Roti",
+    price: 143,
+    img: "https://cdn.pixabay.com/photo/2015/04/13/18/51/fried-721080__340.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Fried rices",
+    price: 57,
+    img: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/300x300/exps9035_RM10002C45D.jpg",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Egg Roti",
+    price: 149,
+    img: "https://cdn.pixabay.com/photo/2016/06/29/16/19/potato-1487166__340.png",
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: "Bean Soup",
+    price: 135,
+    img: "https://cdn.pixabay.com/photo/2016/03/05/21/41/abstract-1239042__340.jpg",
+    amount: 1,
+  },
+  {
+    id: 7,
+    title: "Chicken Soup",
+    price: 693,
+    img: "https://cdn.pixabay.com/photo/2018/09/21/18/25/fillet-3693670__340.jpg",
+    amount: 1,
+  },
+  {
+    id: 8,
+    title: "Vegetable Mix",
+    price: 289,
+    img: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/300x300/exps9035_RM10002C45D.jpg",
+    amount: 1,
+  },
+  {
+    id: 9,
+    title: "Roll-Masala",
+    price: 80,
+    img: "https://cdn.pixabay.com/photo/2016/03/05/21/41/abstract-1239042__340.jpg",
+    amount: 1,
+  },
+  
+  
+  
+];
+
+export default list;
